0775280dc311ea35786f8e31abde1d42af3cb2f9
Elizabeth-41 <fatihearl@gmail.com>
2024-12-10 12:41:54 +0800

    Penambahan Fitur Sorting Array

--- demo01/bMenu/menu.cpp
+++ demo01/bMenu/menu.cpp
@@ -1,63 +1,97 @@
 #include <iostream>
-#include <conio.h>
+#include <string>
 using namespace std;
 
-
-void dMenu(){
-system("cls");
-cout<<"Aplikasi Tampilan Menu"<<"\n";       
-cout<<"1. Menu Pertama"<<"\n";            
-cout<<"2. Menu Kedua"<<"\n";            
-cout<<"3. Menu Ketiga"<<"\n";           
-cout<<"4. Menu Keempat"<<"\n";            
-cout<<"5. Exit"<<"\n";           
-cout<<"Masukan angka :";        
-
+// Fungsi tukar
+void tukar(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-void mPertama(string pesan){
-system("cls");
-cout<<"hallo saya menu "<<pesan;
-getch();
+// Bubble Sort (Ascending)
+void bubble_sort_asc(int* data, int n) {
+    for (int i = 1; i < n; i++) {
+        for (int j = n - 1; j >= i; j--) {
+            if (*(data + j) < *(data + j - 1)) {
+                tukar((data + j), (data + j - 1));
+            }
+        }
+    }
 }
 
+// Bubble Sort (Descending)
+void bubble_sort_desc(int* data, int n) {
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < n - i; j++) {
+            if (*(data + j) < *(data + j + 1)) {
+                tukar((data + j), (data + j + 1));
+            }
+        }
+    }
+}
 
 int main() {
-char pl;
-do
-{
-    dMenu();
-    pl=getch();
-  switch (pl)
-  {
-   case '1':
-    /* code */
-    mPertama("pertama");
-    break;
-   case '2':
-    mPertama("ke- dua");
-    /* code */ 
-    break;  
-   case '3':
-    mPertama("ke- tiga");
-    /* code */
-    break;  
-   case '4':
-    mPertama("ke- empat");
-    /* code */
-    break;  
-  case '5':
-    /* code */
-    break;
-  
-  default:
-    system("cls");
-    cout<<"Pilihan Tidak Tersedia";
-    getch();
-    break;
-  }
+    int* data = new int[100];
+    int n;
 
+    while (true) {
+        cout << "Aplikasi Tampilan Menu\n";
+        cout << "1. Menu Pertama\n";
+        cout << "2. Menu Kedua\n";
+        cout << "3. Menu Ketiga\n";
+        cout << "4. Sorting Array\n";
+        cout << "5. Exit\n";
+        cout << "Masukkan pilihan: ";
+        char pilihan;
+        cin >> pilihan;
 
-} while (pl!='5');
-  return 0;
+        if (pilihan == '4') {
+            cout << "Masukkan jumlah elemen array: ";
+            cin >> n;
+            if (n <= 0) {
+                cout << "Jumlah elemen harus lebih dari 0! Kembali ke menu utama...\n";
+                continue;
+            }
+
+            for (int i = 0; i < n; i++) {
+                cout << "Masukkan elemen ke-" << (i + 1) << ": ";
+                cin >> *(data + i);
+            }
+
+            cout << "Pilih urutan: 1 = Ascending, 2 = Descending: ";
+            char sort_order;
+            cin >> sort_order;
+
+            if (sort_order == '1') {
+                bubble_sort_asc(data, n);
+                cout << "Array setelah diurutkan (Ascending): ";
+            } else if (sort_order == '2') {
+                bubble_sort_desc(data, n);
+                cout << "Array setelah diurutkan (Descending): ";
+            } else {
+                cout << "Pilihan tidak valid. Kembali ke menu utama...\n";
+                continue;
+            }
+
+            for (int i = 0; i < n; i++) {
+                cout << *(data + i) << " ";
+            }
+            cout << endl;
+
+        } else if (pilihan == '5') {
+            break;
+        } else if (pilihan >= '1' && pilihan <= '3') {
+            cout << "Hallo, saya menu ke-" << pilihan << endl;
+        } else {
+            cout << "Pilihan tidak tersedia.\n";
+        }
+
+        cout << "Tekan Enter untuk melanjutkan...\n";
+        cin.ignore();
+        cin.get();
+    }
+
+    delete[] data;
+    return 0;
 }
