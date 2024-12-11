c4d10ea2569cb4ed2bb65493e2dd2285ff5fb3cf
Elizabeth-41 <fatihearl@gmail.com>
2024-12-11 12:38:34 +0800

    Perubahan Ketiga -Asc dan -Desc

--- demo01/bMenu/menu.cpp
+++ demo01/bMenu/menu.cpp
@@ -5,12 +5,44 @@ using namespace std;
 int data[100]; // Array untuk menyimpan data
 int n = 0;     // Jumlah data
 
+// Fungsi untuk menukar elemen
+template<typename T>
+void tukar(T &a, T &b) {
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+// Fungsi untuk sorting ASC
+void sortingASC() {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (data[j] > data[j + 1]) {
+                tukar(data[j], data[j + 1]);
+            }
+        }
+    }
+}
+
+// Fungsi untuk sorting DSC
+void sortingDSC() {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (data[j] < data[j + 1]) {
+                tukar(data[j], data[j + 1]);
+            }
+        }
+    }
+}
+
 void dMenu() {
     system("cls");
-    cout << "Aplikasi Tampilan Menu" << endl;
+    cout << "Aplikasi Sorting Bubble" << endl;
     cout << "1. Masukkan Data" << endl;
     cout << "2. Tampilkan Data" << endl;
-    cout << "3. Exit" << endl;
+    cout << "3. Sorting ASC" << endl;
+    cout << "4. Sorting DSC" << endl;
+    cout << "5. Exit" << endl;
     cout << "Masukkan angka: ";
 }
 
@@ -43,6 +75,26 @@ int main() {
                 getch();
                 break;
             case '3':
+                system("cls");
+                sortingASC();
+                cout << "Data telah diurutkan secara ASC:" << endl;
+                for (int i = 0; i < n; i++) {
+                    cout << data[i] << " ";
+                }
+                cout << endl;
+                getch();
+                break;
+            case '4':
+                system("cls");
+                sortingDSC();
+                cout << "Data telah diurutkan secara DSC:" << endl;
+                for (int i = 0; i < n; i++) {
+                    cout << data[i] << " ";
+                }
+                cout << endl;
+                getch();
+                break;
+            case '5':
                 cout << "Keluar dari aplikasi." << endl;
                 break;
             default:
@@ -50,7 +102,7 @@ int main() {
                 getch();
                 break;
         }
-    } while (pilihan != '3');
+    } while (pilihan != '5');
 
     return 0;
 }
