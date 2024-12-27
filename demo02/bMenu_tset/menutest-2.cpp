045f91b0c8d9ca83daea0409aa7c35b0e7886089
Elizabeth-41 <fatihearl@gmail.com>
2024-12-27 15:34:53 +0800

    Masukkan Data

--- demo02/bMenu_tset/menutest-2.cpp
+++ demo02/bMenu_tset/menutest-2.cpp
@@ -12,3 +12,32 @@ struct mahasiswa {
 
 mahasiswa sikc[30]; // Array untuk menyimpan data mahasiswa
 int pos = -1;       // Penunjuk posisi terakhir data
+
+// Fungsi untuk membersihkan layar (cross-platform)
+void clearScreen() {
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
+
+// Fungsi untuk memasukkan satu data mahasiswa
+void masukanData() {
+    if (pos >= 29) { // Membatasi jumlah maksimal data
+        cout << "Data penuh! Tidak bisa menambahkan data baru.\n";
+        return;
+    }
+    pos++;
+    clearScreen();
+    cout << "Masukan NIM: ";
+    getline(cin, sikc[pos].nim);
+    cout << "Masukan Nama: ";
+    getline(cin, sikc[pos].nama);
+    cout << "Masukan Alamat: ";
+    getline(cin, sikc[pos].alamat);
+    cout << "Masukan IPK: ";
+    cin >> sikc[pos].ipk;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n'); // Membersihkan input buffer
+    cout << "Data berhasil ditambahkan!\n";
+}
